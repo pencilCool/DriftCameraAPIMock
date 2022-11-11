01e8c13ead16c2ecd7156bd4e7f3910d8b6858b7
pencilCool <yhtangcoder@gmail.com>
2022-11-11 09:43:15 +0800

    add foream_mediainfo

--- index.js
+++ index.js
@@ -2,10 +2,47 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/getMediaInfo", (req, res) => {
-  res.type("text/plain");
-  res.send("get MediaInfsss");
-  console.log("");
+app.get("/cgi-bin/foream_mediainfo?mediaInfo", (req, res) => {
+  res.json({
+    files: [
+      {
+        path: "DCIM\\100MEDIA\\VID00001.MP4",
+        size: 7572065,
+        name: "8500005394_1665385272",
+        time: "2021-03-02 23:58:13",
+      },
+      {
+        path: "DCIM\\100MEDIA\\VID00002.MP4",
+        size: 7572065,
+        name: "8500005394_1665385336",
+        time: "2021-03-02 23:59:38",
+      },
+      {
+        path: "DCIM\\100MEDIA\\VID00003.MP4",
+        size: 7572065,
+        name: "8500000001_16653455654",
+        time: "2021-03-02 23:59:38",
+      },
+      {
+        path: "DCIM\\100MEDIA\\VID00004.MP4",
+        size: 7572065,
+        name: "8500000001_16653855654",
+        time: "2021-03-02 23:59:38",
+      },
+      {
+        path: "DCIM\\100MEDIA\\VID00005.MP4",
+        size: 7572065,
+        name: "8500000002_16633855654",
+        time: "2021-03-02 23:59:38",
+      },
+      {
+        path: "DCIM\\100MEDIA\\VID00006.MP4",
+        size: 7572065,
+        name: "8500000002_16635855654",
+        time: "2021-03-02 23:59:38",
+      },
+    ],
+  });
 });
 
 app.use(express.static(__dirname + "SDCard"));
